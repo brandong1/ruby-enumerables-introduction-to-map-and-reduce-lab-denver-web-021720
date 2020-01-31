21deb7929783529c9a1d5b9ddef84da9462d5e8e
Brandon Green <brandon@brandonleeproductions.com>
2020-01-31 22:36:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,7 +21,14 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  array = []
+  index = 0 
   
+  while index < source_array.size do 
+    array.push(source_array[index])
+    index += 1 
+  end
+  return array
 end
 
 def map_to_square(source_array)
