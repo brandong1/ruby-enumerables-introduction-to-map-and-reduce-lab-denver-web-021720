1e529fe9073e1b98ddc7aa56425ddba73a390b46
Brandon Green <brandon@brandonleeproductions.com>
2020-01-31 22:33:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,41 @@
-# My Code here....
+def map_to_negativize(source_array)
+  array = []
+  index = 0 
+  
+  while index < source_array.size do 
+    array.push(source_array[index] * -1)
+    index += 1 
+  end
+  array
+end
+
+def map_to_no_change(source_array)
+  array = []
+  index = 0 
+  
+  while index < source_array.size do 
+    array.push(source_array[index])
+    index += 1 
+  end
+  return array
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
