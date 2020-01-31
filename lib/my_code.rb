62322a2a1c920770be9d450f1482c77b9b0503d4
Brandon Green <brandon@brandonleeproductions.com>
2020-01-31 22:45:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,7 +46,7 @@ def map_to_square(source_array)
   return array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   new = starting_point
   i = 0
   
